719bd424228b5120057c5cd23f95c52f46003ab3
Zero <stalker_mr@list.ru>
2024-04-15 23:07:28 +0900

    Update

---
+++ UserControls/Playlist.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicPlayer.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Playkist.xaml
+    /// </summary>
+    public partial class Playkist : UserControl
+    {
+        public Playkist()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register
+            ("Title", typeof(string), typeof(Playkist));
+
+        public string Desc
+        {
+            get { return (string)GetValue(DescProperty); }
+            set { SetValue(DescProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescProperty = DependencyProperty.Register
+            ("Desc", typeof(string), typeof(Playkist));
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register
+            ("Image", typeof(ImageSource), typeof(Playkist));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register
+             ("IsActive", typeof(bool), typeof(Playkist));
+    }
+}
